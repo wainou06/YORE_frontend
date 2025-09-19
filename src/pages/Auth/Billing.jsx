71d5156c25b00e335a843b363cf34af1681382d2
aggawa <whyd_o@naver.com>
2025-09-19 17:32:57 +0900

    [전준모] style : 사진 파일 경로 수정

--- src/pages/Auth/Billing.jsx
+++ src/pages/Auth/Billing.jsx
@@ -52,7 +52,7 @@ const Billing = () => {
                      </div>
 
                      <div className="col-12 col-md-4 d-none d-md-flex align-items-start justify-content-center">
-                        <img src="/src/assets/images/billing.jpg" alt="세팅 이미지" className="img-fluid responsive-settings" />
+                        <img src="../../../public/images/billing.jpg" alt="청구서 이미지" className="img-fluid responsive-settings" />
                      </div>
                   </div>
                </div>
