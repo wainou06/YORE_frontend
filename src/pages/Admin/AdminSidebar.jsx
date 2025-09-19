64367d700982916456b024060c9f2a45e093a26d
blessserenity <love060214@naver.com>
2025-09-19 13:19:58 +0900

    [차민준] ✨ feat: 퍼센트 추가

--- src/pages/Admin/AdminSidebar.jsx
+++ src/pages/Admin/AdminSidebar.jsx
@@ -15,6 +15,7 @@ export const AdminSidebar = () => {
    const notifications = useSelector((state) => state.notification.notifications)
    const unreadCount = notifications.filter((n) => !n.isRead).length
    const dispatch = useDispatch()
+   const modal = useSelector((state) => state.modal)
 
    useEffect(() => {
       const pathname = window.location.pathname
@@ -37,88 +38,90 @@ export const AdminSidebar = () => {
    }, [])
 
    return (
-      <div className="admin-sidebar">
-         <div className="sidebar-content">
-            <div className="sidebar-header d-flex justify-content-between">
-               <Link to="/admin" onClick={() => setCurrentPage(0)} className="logo">
-                  YORE
-               </Link>
-               <NotificationDropdown show={showDropdown} onClose={() => setShowDropdown(false)} onToggle={() => setShowDropdown((prev) => !prev)} notifications={notifications} unreadCount={unreadCount} />
-            </div>
-            <nav className="sidebar-menu">
-               {currentPage === 0 ? (
-                  <Link to="/admin" className="menu-item active">
-                     <FontAwesomeIcon icon={faHome} />
-                     <span>홈</span>
+      <>
+         <div className="admin-sidebar">
+            <div className="sidebar-content">
+               <div className="sidebar-header d-flex justify-content-between">
+                  <Link to="/admin" onClick={() => setCurrentPage(0)} className="logo">
+                     YORE
                   </Link>
-               ) : (
-                  <Link onClick={() => setCurrentPage(0)} to="/admin" className="menu-item">
-                     <FontAwesomeIcon icon={faHome} />
-                     <span>홈</span>
-                  </Link>
-               )}
-               {currentPage === 1 ? (
-                  <Link to="/admin/users" className="menu-item active">
-                     <FontAwesomeIcon icon={faUsers} />
-                     <span>사용자</span>
-                  </Link>
-               ) : (
-                  <Link onClick={() => setCurrentPage(1)} to="/admin/users" className="menu-item">
-                     <FontAwesomeIcon icon={faUsers} />
-                     <span>사용자</span>
-                  </Link>
-               )}
-               {currentPage === 2 ? (
-                  <Link to="/admin/plans" className="menu-item active">
-                     <FontAwesomeIcon icon={faCreditCard} />
-                     <span>요금제</span>
-                  </Link>
-               ) : (
-                  <Link onClick={() => setCurrentPage(2)} to="/admin/plans" className="menu-item">
-                     <FontAwesomeIcon icon={faCreditCard} />
-                     <span>요금제</span>
-                  </Link>
-               )}
-               {currentPage === 3 ? (
-                  <Link to="/admin/orders" className="menu-item active">
-                     <FontAwesomeIcon icon={faShoppingCart} />
-                     <span>결제</span>
-                  </Link>
-               ) : (
-                  <Link onClick={() => setCurrentPage(3)} to="/admin/orders" className="menu-item">
-                     <FontAwesomeIcon icon={faShoppingCart} />
-                     <span>결제</span>
-                  </Link>
-               )}
-            </nav>
-            <div className="sidebar-footer">
-               <div
-                  className="menu-item"
-                  onClick={() => {
-                     const newDarkMode = !darkMode
-                     setDarkMode(newDarkMode)
-                     document.documentElement.setAttribute('data-theme', newDarkMode ? 'dark' : 'light')
-                     localStorage.setItem('theme', newDarkMode ? 'dark' : 'light')
-                  }}
-                  style={{ cursor: 'pointer' }}
-               >
-                  <FontAwesomeIcon icon={faMoon} />
-                  <span>다크 모드</span>
+                  <NotificationDropdown show={showDropdown} onClose={() => setShowDropdown(false)} onToggle={() => setShowDropdown((prev) => !prev)} notifications={notifications} unreadCount={unreadCount} />
                </div>
-               <Link
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => {
-                     localStorage.removeItem('adminToken')
-                     sessionStorage.removeItem('adminToken')
-                  }}
-                  className="menu-item"
-                  to={'/'}
-               >
-                  <FontAwesomeIcon icon={faRightFromBracket} />
-                  <span>로그아웃</span>
-               </Link>
+               <nav className="sidebar-menu">
+                  {currentPage === 0 ? (
+                     <Link to="/admin" className="menu-item active">
+                        <FontAwesomeIcon icon={faHome} />
+                        <span>홈</span>
+                     </Link>
+                  ) : (
+                     <Link onClick={() => setCurrentPage(0)} to="/admin" className="menu-item">
+                        <FontAwesomeIcon icon={faHome} />
+                        <span>홈</span>
+                     </Link>
+                  )}
+                  {currentPage === 1 ? (
+                     <Link to="/admin/users" className="menu-item active">
+                        <FontAwesomeIcon icon={faUsers} />
+                        <span>사용자</span>
+                     </Link>
+                  ) : (
+                     <Link onClick={() => setCurrentPage(1)} to="/admin/users" className="menu-item">
+                        <FontAwesomeIcon icon={faUsers} />
+                        <span>사용자</span>
+                     </Link>
+                  )}
+                  {currentPage === 2 ? (
+                     <Link to="/admin/plans" className="menu-item active">
+                        <FontAwesomeIcon icon={faCreditCard} />
+                        <span>요금제</span>
+                     </Link>
+                  ) : (
+                     <Link onClick={() => setCurrentPage(2)} to="/admin/plans" className="menu-item">
+                        <FontAwesomeIcon icon={faCreditCard} />
+                        <span>요금제</span>
+                     </Link>
+                  )}
+                  {currentPage === 3 ? (
+                     <Link to="/admin/orders" className="menu-item active">
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        <span>결제</span>
+                     </Link>
+                  ) : (
+                     <Link onClick={() => setCurrentPage(3)} to="/admin/orders" className="menu-item">
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        <span>결제</span>
+                     </Link>
+                  )}
+               </nav>
+               <div className="sidebar-footer">
+                  <div
+                     className="menu-item"
+                     onClick={() => {
+                        const newDarkMode = !darkMode
+                        setDarkMode(newDarkMode)
+                        document.documentElement.setAttribute('data-theme', newDarkMode ? 'dark' : 'light')
+                        localStorage.setItem('theme', newDarkMode ? 'dark' : 'light')
+                     }}
+                     style={{ cursor: 'pointer' }}
+                  >
+                     <FontAwesomeIcon icon={faMoon} />
+                     <span>다크 모드</span>
+                  </div>
+                  <Link
+                     style={{ cursor: 'pointer' }}
+                     onClick={() => {
+                        localStorage.removeItem('adminToken')
+                        sessionStorage.removeItem('adminToken')
+                     }}
+                     className="menu-item"
+                     to={'/'}
+                  >
+                     <FontAwesomeIcon icon={faRightFromBracket} />
+                     <span>로그아웃</span>
+                  </Link>
+               </div>
             </div>
          </div>
-      </div>
+      </>
    )
 }
